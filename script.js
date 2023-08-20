1054a4d14833801a29b8298a0427136a61538b6c
Marvin <cecile.pilorger@gmail.com>
2023-08-20 16:35:02 +0200

    corection

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let myForm = document.getElementById("myform");
 
 let firstName = document.getElementById("first-name");
@@ -24,13 +25,7 @@ function checkIfEmpty() {
 // au sein de la liste déjà présente(en reprenant les balises HTML d’un des commentaires).
 
 function addNewComment() {
-  // Récupérer les valeurs de chaque champ
- console.log(firstName.value);
- console.log(lastName.value);
- console.log(message.value);
-
- // Je vérifie si j'ai bien ma liste de commentaires
- console.log(myComment);
+  
  // si tous les valeurs de chaque champs est supérieur à ""
  if (firstName.value.trim() > "" && lastName.value.trim() >  "" && message.value.trim() > ""){
  // Créer l'élément 
@@ -38,21 +33,19 @@ function addNewComment() {
  let deuxiemeDiv = document.createElement("div");
  let myTitre = document.createElement("h3");
  let troisiemeDiv = document.createElement("div");
- //let myPara =document.createElement("p");
+ 
  
  // Ajoute les classes
  premiereDiv.classList.add("flex","space-x-4","text-sm","text-gray-500", "border-gray-200");
- console.log(premiereDiv);
+
  deuxiemeDiv.classList.add("flex-1", "py-10", "border-t", "border-gray-200");
- console.log(deuxiemeDiv);
+
   myTitre.classList.add("font-medium","text-gray-900");
- console.log(myTitre);
+
  troisiemeDiv.classList.add("prose", "prose-sm", "mt-4", "max-w-none", "text-gray-500");
- console.log(troisiemeDiv);
+
 
  //  contenu
-  premiereDiv.textContent = "";
-  deuxiemeDiv.textContent = "" ;
   myTitre.textContent = firstName.value +" "+lastName.value+ "" ;
   troisiemeDiv.textContent = message.value;
    
@@ -91,4 +84,5 @@ myForm.addEventListener("submit", e => {
     
 })
       
+
 
